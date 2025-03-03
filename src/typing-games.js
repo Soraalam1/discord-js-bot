01c69fe1db0ee9ed64e19cbe5259dd8a27c3c14f
soraalam1 <s1gamingc@gmail.com>
2025-03-03 00:05:37 -0500

    fix yugioh infinite game glitch

--- src/typing-games.js
+++ src/typing-games.js
@@ -35,7 +35,7 @@ const POKEMON_GEN_MAP = {
     6: [650, 721],
     7: [722, 807]
 }
-//test
+
 let correctAnswer;
 let numberOfRounds = 0;
 let genRestriction = null;
@@ -122,7 +122,7 @@ const postReady = async (interaction) => {
             if (embedData.answer.embed) {
                 await discordChannel.send({
                     embeds: [embedData.answer.embed.setTimestamp(new Date()).toJSON()],
-                    files: [embedData.answer.attachment]
+                    files: embedData.answer.attachment ? [embedData.answer.embed] : []
                 })
             }
 
